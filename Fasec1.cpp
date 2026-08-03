5476628cec36da4819e4782ea9d185e4fe3f43ec
SanH3x <heitorvicente_japa@outlook.com>
2026-08-03 12:27:50 -0300

    Code1: Calculadora de Notas, Construção e Compilação

---
+++ Fasec1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+//Código que utilza 2 a 3 notas de uma aluno para o cálculo da média
+
+
+double calcularMedia(double a, double b) {
+    return (a + b) / 2.0;
+}
+
+double calcularMedia(double a, double b, double c) {
+    return (a + b + c) / 3.0;
+}
+
+int main() {
+    std::string nome = "Heitor";
+    double nota1 = 6.5, nota2 = 8.5, nota3 = 7.0;
+    
+    std::cout << "Aluno: " << nome << "\n";
+    std::cout << "Media: " << calcularMedia(nota1, nota2) << "\n";
+    std::cout << "Media Secundaria: " << calcularMedia(nota1, nota2, nota3) << "\n";
+
+    return 0;
+}
